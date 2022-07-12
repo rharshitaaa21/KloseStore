3bdfb9860da199e19ddb8e5b13260be9cb43a578
Harshita Raghav <raghav.harshita21@gmail.com>
2022-07-12 11:45:06 +0530

    Initial Commit

---
+++ lib/providers/auth.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
+import '../models/http_exception.dart';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/$urlSegment?key=[API_KEY]');
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'accounts:signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'accounts:signInWithPassword');
+  }
+}
